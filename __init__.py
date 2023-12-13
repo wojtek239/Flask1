af4c0d0149848486a0f90de05a1e5fe5b0623b7a
wojtek239 <wojtekopelt1@wp.pl>
2023-12-13 18:17:55 +0100

    lesson 3 flask

--- __init__.py
+++ __init__.py
@@ -1,15 +1,21 @@
 from flask import Flask
 from hashlib import md5
 
+
 def create_app():
     app = Flask(__name__)
     encryptor = md5()
     app.debug = True
     app.secret_key = encryptor.digest()
 
-from .main import login_blueprint, dashboard_blueprint, logout_blueprint
+    from main import login_blueprint, dashboard_blueprint, logout_blueprint
     app.register_blueprint(login_blueprint)
     app.register_blueprint(dashboard_blueprint)
     app.register_blueprint(logout_blueprint)
 
     return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host='0.0.0.0', port=8000)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, Blueprint, request, session ### 0
+from flask import render_template, redirect, url_for, Blueprint, request, session
 
 login_blueprint = Blueprint("login", __name__)
 dashboard_blueprint = Blueprint("dashboard", __name__)
@@ -8,7 +8,7 @@ logout_blueprint = Blueprint("logout", __name__)
 @login_blueprint.route('/', methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        session.update({"nick" : request.form['nickname']}) ### 1
+        session.update({"nick": request.form['nickname']})
     elif request.method == "GET" and "nick" not in session:
         return render_template("login.html")
 
@@ -18,15 +18,14 @@ def login():
 @logout_blueprint.route('/logout')
 def logout():
     if "nick" in session:
-        session.pop("nick") ### 2
+        session.pop("nick")
 
     return redirect(url_for("login.login"))
 
 
 @dashboard_blueprint.route('/dashboard')
-
 def dashboard():
-    if "nick" in session: ### 3
+    if "nick" in session:
         return render_template("dashboard.html", nickname=session["nick"])
 
     return redirect(url_for("login.login"))
